27505fe788495b7aaaf13c7cfba8c86abdaaf543
Rohit Kumar Vishwakarma <rohitvishwakarma8082@gmail.com>
2026-04-07 18:04:14 +0000

    This is initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT ?? 8080
+
+app.get('/', (req, res) => {
+    return res.json({ msg: 'Hello from the server v5 for app final deploy\n' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`)
+})
